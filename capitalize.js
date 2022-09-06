9eb054bd8d41ba4d96984ab2b98e29294604a8d1
doozles411 <cindy.phelps01@gmail.com>
2022-09-06 13:04:50 -0700

    Create capitalize function

--- capitalize.js
+++ capitalize.js
@@ -1,5 +1,11 @@
-function sum(a, b) {
-    return a + b;
+function capitalize(str) {
+    if (str.length === 0) {
+        return '';
+    }
+
+    const firstLetter = str.slice(0, 1).toUpperCase();
+    const allOthers = str.slice(1, str.length).toLowerCase();
+    return firstLetter + allOthers;
 }
 
-export default sum;
+export default capitalize;
