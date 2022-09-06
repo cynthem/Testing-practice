1c61c49617ef982e2013b7e5d2a2f33b30cc4c36
doozles411 <cindy.phelps01@gmail.com>
2022-09-06 13:27:07 -0700

    Create and run three tests

--- tests/reverseString.test.js
+++ tests/reverseString.test.js
@@ -1,1 +1,13 @@
 import reverseString from "../reverseString";
+
+test('hello to be olleh', () => {
+    expect(reverseString('hello')).toBe('olleh');
+});
+
+test('Oh hi there! to be !ereht ih hO', () => {
+    expect(reverseString('Oh hi there!')).toBe('!ereht ih hO');
+});
+
+test('"" to be ""', () => {
+    expect(reverseString("")).toBe("");
+});
